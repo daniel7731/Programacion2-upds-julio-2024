3809f300b64ea2d613ebab013dfb3f0bf5cdfeae
Daniel Roman <droman@gruposion.bo>
2024-07-07 22:29:19 -0400

    actualizacion de ejercicios

--- ConsoleApp1/ConsoleApp1/Program.cs
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -15,17 +15,54 @@ namespace ConsoleApp1
         }
         public Boolean esPrimo( int numero)
         {
-            return false;
+            if (numero <= 1)
+            {
+                return false;
+            }
+            else if (numero <= 3)
+            {
+                return true;
+            }
+            else if (numero % 2 == 0)
+            {
+                return false;
+            }
+
+            // Check for odd divisors from 3 to sqrt(number)
+            int i = 3;
+            while (i * i <= numero)
+            {
+                if (numero % i == 0)
+                {
+                    return false;
+                }
+
+                i += 2;
+            }
+
+            return true;
         }
         public Boolean esMutiplo6(int numero) { 
         
-            return false;
+            return numero % 6 == 0;
         }
-        public int mayorEntre3Numeros(int numero) { 
-        
-            return 0;
+        public int mayorEntre3Numeros(int numero1, int numero2, int numero3 ) {
+
+            int mayor = numero1;
+
+            if (numero2 > mayor)
+            {
+                mayor = numero2;
+            }
+
+            if (numero3 > mayor)
+            {
+                mayor = numero3;
+            }
+
+            return mayor;
         }
-        public int mayorEntre4Numeros (int numero)
+        public int mayorEntre4Numeros (int numero1, int numero2, int numero3 , int numero4)
         {
             return 0;
         }
@@ -33,26 +70,86 @@ namespace ConsoleApp1
         {
 
             Program p = new Program();
-            int x = 5;
-            Boolean si = p.esPar(x);
-            System.Console.WriteLine(x);
-            if ( si)
-            {
-                Console.WriteLine("Es Par");
-            }
-            else {
-
-                Console.WriteLine("Es impar");
-            }
+            
             while(true)
             {
-                Console.WriteLine("finalizar? S/N");
-                string comando = Console.ReadLine();
-                if (comando == "S")
-                    break;
-                Ejercicios.Ejercicio1.calcular(args);
-                Ejercicios.Ejercicio2.calcular(args);
-                Ejercicios.Ejercicio3.calcular(args);
+                Console.WriteLine("¡Bienvenido al programa de utilidades!");
+                Console.WriteLine("Elige una de las siguientes opciones:\r\n\r\n 1.Determinar si un número es primo.\r\n 2.Determinar si un número es par.\r\n 3.Determinar si un número es múltiplo de 6.\r\n 4.Encontrar el número mayor entre tres números.\r\n 5.Encontrar el número mayor entre cuatro números.");
+
+             
+                try
+                {
+                    string comando1 = Console.ReadLine();
+                    if (comando1.Equals("2"))
+                    {
+                        Console.WriteLine("Ingresa un número");
+                        int num = int.Parse( Console.ReadLine());
+                        Boolean bandera = p.esPar(num);
+                        if ( bandera)
+                            Console.WriteLine("El número " + num +" es par");
+                        else
+                            Console.WriteLine("El número " + num + " no es par");
+                    }
+                    if (comando1.Equals("3"))
+                    {
+                        Console.WriteLine("Ingresa un número");
+                        int num = int.Parse(Console.ReadLine());
+                        Boolean bandera = p.esMutiplo6(num);
+                        if (bandera)
+                            Console.WriteLine("El número " + num + " es multiplo de 6");
+                        else
+                            Console.WriteLine("El número " + num + " no es multiplo de 6");
+                    }
+                    if (comando1.Equals("1"))
+                    {
+                        Console.WriteLine("Ingresa un número");
+                        int num = int.Parse(Console.ReadLine());
+                        Boolean bandera = p.esPrimo(num);
+                        if (bandera)
+                            Console.WriteLine("El número " + num + " es primo");
+                        else
+                            Console.WriteLine("El número " + num + " no es primo");
+                    }
+                    if (comando1.Equals("4"))
+                    {
+                        Console.WriteLine("Ingresa el primer número");
+                        int num1 = int.Parse(Console.ReadLine());
+                        Console.WriteLine("Ingresa el segundo número");
+                        int num2 = int.Parse(Console.ReadLine());
+                        Console.WriteLine("Ingresa el tercero número");
+                        int num3 = int.Parse(Console.ReadLine());
+                        Console.WriteLine("Ingresa el tercero número");
+                        int mayor = p.mayorEntre3Numeros(num1, num2, num3);
+                        Console.WriteLine("El número " + mayor + " es el mayor entre ("+num1+','+num2+','+num3+")");
+                    }
+                    if (comando1.Equals("5"))
+                    {
+                        Console.WriteLine("Ingresa el primer número");
+                        int num1 = int.Parse(Console.ReadLine());
+                        Console.WriteLine("Ingresa el segundo número");
+                        int num2 = int.Parse(Console.ReadLine());
+                        Console.WriteLine("Ingresa el tercero número");
+                        int num3 = int.Parse(Console.ReadLine());
+                        Console.WriteLine("Ingresa el cuarto número");
+                        int num4 = int.Parse(Console.ReadLine());
+                        int mayor = p.mayorEntre4Numeros(num1,num2, num3, num4);
+                        Console.WriteLine("El número " + mayor + " es el mayor entre (" + num1 + ',' + num2 + ',' + num3 +","+num4 +")");
+                    }
+
+                    Console.WriteLine("Escribe S para finalizar");
+                    string comando = Console.ReadLine();
+                    comando = comando.Trim();
+                    comando = comando.ToLower();
+                    if ( comando.Equals("s")) {
+                        break;
+                    }
+                }
+                catch(Exception ex)
+                {
+
+                }
+
+               
             }
         }
     }
---
+++ EstructuraCondicionalSimple1/EstructuraCondicionalSimple1/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EstructuraCondicionalSimple1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int num1, num2;
+            string linea;
+            Console.Write("Ingrese primer valor:");
+            linea = Console.ReadLine();
+            num1 = int.Parse(linea);
+            Console.Write("Ingrese segundo valor:");
+            linea = Console.ReadLine();
+            num2 = int.Parse(linea);
+            if (num1 > num2)
+            {
+                Console.Write(num1);
+            }
+            else
+            {
+                Console.Write(num2);
+            }
+            Console.ReadKey();
+        }
+    }
+}
