c0d582c2f16bd4284b07cce311eb53e89793cbd9
Daniel Roman <droman@gruposion.bo>
2024-07-11 17:41:18 -0400

    arreglos y vectores

---
+++ 2/Vector1/Vector1/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Vector1
+{
+    internal class Program
+    {
+        private int[] sueldos;
+
+        public void Cargar()
+        {
+            sueldos = new int[5];
+            for (int f = 0; f < 5; f++)
+            {
+                Console.Write("Ingrese valor de la componente:");
+                String linea;
+                linea = Console.ReadLine();
+                sueldos[f] = int.Parse(linea);
+            }
+        }
+
+        public void Imprimir()
+        {
+            for (int f = 0; f < 5; f++)
+            {
+                Console.WriteLine(sueldos[f]);
+            }
+            Console.ReadKey();
+        }
+        static void Main(string[] args)
+        {
+            Program pv = new Program();
+            pv.Cargar();
+            pv.Imprimir();
+        }
+    }
+}
--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 # Programacion2-upds-julio-2024
+1.-Entornos y herramientas de programación
+2.-Arreglos y matrices.
+3.-Registros(Clases)
+4.-Archivos
