d999a1e10f360ed085c55f9b211ea4c76f4fc785
Daniel Roman <droman@gruposion.bo>
2024-07-15 14:10:19 -0400

    matrices

---
+++ 2/Matrices/Matrices/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Matrices
+{
+    internal class Matriz
+    {
+        private int[,] mat;
+        public void Cargar()
+        {
+            mat = new int[3, 5];
+            for (int f = 0; f < 3; f++)
+            {
+                for (int c = 0; c < 5; c++)
+                {
+                    Console.Write("Ingrese componente:");
+                    string linea;
+                    linea = Console.ReadLine();
+                    mat[f, c] = int.Parse(linea);
+                }
+            }
+        }
+
+        public void Imprimir()
+        {
+            for (int f = 0; f < 3; f++)
+            {
+                for (int c = 0; c < 5; c++)
+                {
+                    Console.Write(mat[f, c] + " ");
+                }
+                Console.WriteLine();
+            }
+            Console.ReadKey();
+        }
+
+        static void Main(string[] args)
+        {
+            Matriz ma = new Matriz();
+            ma.Cargar();
+            ma.Imprimir();
+        }
+    }
+}
---
+++ 2/Matrices2/Matrices2/Program.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Matrices2
+{
+    public class Matriz
+    {
+        private int[,] mat;
+        public int filas { get;   }
+        public int columnas { get; }
+        public Matriz(int filas, int columnas)
+        {
+            this.filas = Math.Abs( filas);
+            this.columnas = Math.Abs(columnas);
+            mat = new int[filas, columnas];
+        }
+        public void CargarAleatorio(int minimo, int maximo)
+        {
+          Random random = new Random();
+            for (int f = 0; f < filas; f++)
+            {
+                for (int c = 0; c < columnas; c++)
+                {
+              
+                   
+                    mat[f, c] = random.Next(minimo,maximo);
+                }
+            }
+        }
+
+        public void Imprimir()
+        {
+            for (int f = 0; f < filas; f++)
+            {
+                for (int c = 0; c < columnas; c++)
+                {
+                    Console.Write(mat[f, c] + " ");
+                }
+                Console.WriteLine("");
+            }
+        }
+
+        static void Main(string[] args)
+        {
+            try
+            {
+                Console.WriteLine("Ingresa el numero de filas :");
+                int filas = int.Parse(Console.ReadLine());
+                Console.WriteLine("Ingresa el numero de filas :");
+                int columnas = int.Parse(Console.ReadLine());
+                Matriz m = new Matriz(filas, columnas);
+                m.CargarAleatorio(1,10);
+                m.Imprimir();
+            }
+            catch (Exception e){ 
+            
+            }
+            Console.WriteLine("Presiona una tecla para finalizar");
+            Console.ReadKey();
+
+        }
+       
+    }
+}
