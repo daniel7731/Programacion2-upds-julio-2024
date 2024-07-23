2f1a33579f34bc2e7b7a751a71935c81298deb16
Daniel Roman <droman@gruposion.bo>
2024-07-23 17:10:41 -0400

    archivos

---
+++ 4/Archivos/WindowsFormsApp1/FArchivos.Designer.cs
@@ -0,0 +1,131 @@
+﻿namespace WindowsFormsApp1
+{
+    partial class FArchivos
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lecturaTb = new System.Windows.Forms.TextBox();
+            this.escrituratb = new System.Windows.Forms.TextBox();
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.saveFileDialog1 = new System.Windows.Forms.SaveFileDialog();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // lecturaTb
+            // 
+            this.lecturaTb.Location = new System.Drawing.Point(58, 82);
+            this.lecturaTb.Multiline = true;
+            this.lecturaTb.Name = "lecturaTb";
+            this.lecturaTb.ReadOnly = true;
+            this.lecturaTb.Size = new System.Drawing.Size(287, 233);
+            this.lecturaTb.TabIndex = 0;
+            // 
+            // escrituratb
+            // 
+            this.escrituratb.Location = new System.Drawing.Point(420, 82);
+            this.escrituratb.Multiline = true;
+            this.escrituratb.Name = "escrituratb";
+            this.escrituratb.Size = new System.Drawing.Size(287, 233);
+            this.escrituratb.TabIndex = 1;
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.FileName = "openFileDialog1";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(55, 57);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(81, 13);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Leer un archivo";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(417, 57);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(94, 13);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "Escribir un archivo";
+            this.label2.Click += new System.EventHandler(this.label2_Click);
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(131, 340);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 4;
+            this.button1.Text = "Abrir Archivo";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(534, 340);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(102, 23);
+            this.button2.TabIndex = 5;
+            this.button2.Text = "Guardar Archivo";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // FArchivos
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.escrituratb);
+            this.Controls.Add(this.lecturaTb);
+            this.Name = "FArchivos";
+            this.Text = "FArchivos";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox lecturaTb;
+        private System.Windows.Forms.TextBox escrituratb;
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+        private System.Windows.Forms.SaveFileDialog saveFileDialog1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+    }
+}
+
---
+++ 4/Archivos/WindowsFormsApp1/FArchivos.cs
@@ -0,0 +1,107 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp1
+{
+    public partial class FArchivos : Form
+    {
+        public FArchivos()
+        {
+            InitializeComponent();
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+
+        }
+        private bool escribirArchivo(String ruta)
+        {
+            try
+            {
+                // Abrir el archivo para escritura
+
+                StreamWriter sw = new StreamWriter(ruta);
+
+                // Escribir el contenido de la variable en el archivo
+                string contenido = escrituratb.Text;
+                string[] lineas = contenido.Split('\n');
+                foreach (string linea in lineas)
+                {
+                    sw.WriteLine(linea);
+
+                }
+
+                // Cerrar el archivo
+                sw.Close();
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("Error al escribir el archivo: " + e.Message);
+                return false;
+            }
+            return true;
+        }
+        private void leerArchivo(String ruta)
+        {
+            try
+            {
+                // Abrir el archivo para lectura
+                StreamReader sr = new StreamReader(ruta);
+
+                // Leer cada línea del archivo
+                string linea;
+                string texto="";
+                while ((linea = sr.ReadLine()) != null)
+                {
+                    texto += linea;
+                    //Console.WriteLine(linea);
+                }
+
+                // Cerrar el archivo
+                sr.Close();
+                lecturaTb.Text = texto;
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("Error al leer el archivo: " + e.Message);
+            }
+        }
+        private void button1_Click(object sender, EventArgs e)
+        {
+            DialogResult openFile = openFileDialog1.ShowDialog(this);
+            if ( openFile == DialogResult.OK)
+            {
+
+                string fileName = openFileDialog1.FileName;
+                leerArchivo(fileName);
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            DialogResult openFile = saveFileDialog1.ShowDialog();
+            if (openFile == DialogResult.OK)
+            {
+
+                string fileName = saveFileDialog1.FileName;
+                bool exito = escribirArchivo(fileName);
+                if (exito)
+                {
+                    MessageBox.Show("Archivo : " + fileName + " guardado correctamente!");
+                }
+                else
+                {
+                    MessageBox.Show("Error guardando archivo");
+                }
+            }
+        }
+    }
+}
